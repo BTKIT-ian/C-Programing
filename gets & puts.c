5bca986d1f655efe9690fde9f624ba26c6ac6032
Gaurav Fulara <77954499+BTKIT-ian@users.noreply.github.com>
2021-01-27 13:55:09 +0530

    Add files via upload

---
+++ gets & puts.c
@@ -0,0 +1,11 @@
+		#include<stdio.h>
+		void main()
+{
+	int i;
+	char s[20]="GAURAV";    
+	printf("\n Enter the name");
+	gets(s); /*The gets() allows the user to enter the space-separated strings.*/  
+	   puts(s); /*The puts() function is used to print the string on the console which is 
+	               previously read by using gets() or scanf() function.*/
+	getch();
+}
