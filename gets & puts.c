4512102890ecebeae9860787ece34eb6b19c9fbd
Gaurav Fulara <77954499+BTKIT-ian@users.noreply.github.com>
2021-01-27 13:56:42 +0530

    Update gets & puts.c

--- gets & puts.c
+++ gets & puts.c
@@ -3,7 +3,7 @@
 {
 	int i;
 	char s[20]="GAURAV";    
-	printf("\n Enter the name");
+	printf("\n Enter the name=");
 	gets(s); /*The gets() allows the user to enter the space-separated strings.*/  
 	   puts(s); /*The puts() function is used to print the string on the console which is 
 	               previously read by using gets() function.*/
